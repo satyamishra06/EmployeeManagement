c293dddb4a5d3e41b1c7ba601e287f8d620780aa
satya-mishra <302017286+satyamishra06@users.noreply.github.com>
2026-09-19 17:57:52 +0530

    "feat: configure Entity Framework Core and database"

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+using EmployeeManagement.Data;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddOpenApi();
